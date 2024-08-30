1ac49368b652e363f340753b2b35e7a8555af290
SarfarajMolla <sarfarajmollah49@gmail.com>
2024-08-30 23:07:27 +0530

    Load products in beforeAll hook

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,23 +1,35 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
-import {  cart } from "../../data/cart-class.js";
+import { cart } from "../../data/cart-class.js";
+import { loadProducts } from "../../data/products.js";
+
 describe("test suite: renderOrderSummary", () => {
   const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
   const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
+
+  beforeAll((done) => {
+    loadProducts(()=>{
+       done();
+    });
+  });
+
   beforeEach(() => {
     spyOn(localStorage, "setItem");
     document.querySelector(".js-test-container").innerHTML = `
       <div class="js-order-summary"></div>
       <div class="js-payment-summary"></div>
     `;
-    cart.cartItems = [{
-      productId: productId1,
-      quantity: 2,
-      deliveryOptionId: '1'
-    }, {
-      productId: productId2,
-      quantity: 1,
-      deliveryOptionId: '2'
-    }];
+    cart.cartItems = [
+      {
+        productId: productId1,
+        quantity: 2,
+        deliveryOptionId: "1",
+      },
+      {
+        productId: productId2,
+        quantity: 1,
+        deliveryOptionId: "2",
+      },
+    ];
     renderOrderSummary();
   });
   afterEach(() => {
