3e939bbab702cdeca6a83d166c000c712780b29b
SarfarajMolla <sarfarajmollah49@gmail.com>
2024-08-30 22:10:19 +0530

    Add backend practise file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,6 +3,7 @@ import {renderPaymentSummary} from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 //import '../data/car.js';
 //import '../data/cart-class.js';
+import '../data/backend-practise.js';
 
 renderCheckoutHeader();
 renderOrderSummary();
