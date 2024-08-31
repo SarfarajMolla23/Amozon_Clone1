35aacbc8d71a780c168f19b73fae20d446c67947
SarfarajMolla <sarfarajmollah49@gmail.com>
2024-08-31 19:00:01 +0530

    Learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,14 +1,61 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
-import {loadProducts} from '../data/products.js';
+import { loadProducts } from "../data/products.js";
+import {loadCart} from '../data/cart.js';
+
 //import '../data/car.js';
 //import '../data/cart-class.js';
 //import '../data/backend-practise.js';
 
-loadProducts(() => {
+
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve('value1');
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(()=>{
+      resolve();
+    });
+   })
+
+]).then((values)=>{
   renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('value1');
+  });
+
+}).then((value) => {
+  console.log(value);
+
+return new Promise((resolve) => {
+  loadCart(()=>{
+    resolve();
+  });
+ });
+
+}).then(()=>{
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
+
+/*
+loadProducts(() => {
+  loadCart(()=>{
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+});
+*/
 
