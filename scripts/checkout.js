806e28dfe26bcc8e5f9f7666aa03d9f149be7918
SarfarajMolla <sarfarajmollah49@gmail.com>
2024-08-31 21:34:22 +0530

    Convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,17 +1,32 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
-import { loadProducts,loadProductsFetch } from "../data/products.js";
-import {loadCart} from '../data/cart.js';
+import { loadProducts, loadProductsFetch } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 
 //import '../data/car.js';
 //import '../data/cart-class.js';
 //import '../data/backend-practise.js';
 
+async function loadPage() {
+  await loadProductsFetch();
 
+  const value = await new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+loadPage();
+
+/*
 Promise.all([
  loadProductsFetch(),
- 
+
   new Promise((resolve) => {
     loadCart(()=>{
       resolve();
@@ -23,6 +38,7 @@ Promise.all([
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
 
 /*
 new Promise((resolve) => {
@@ -55,4 +71,3 @@ loadProducts(() => {
   });
 });
 */
-
