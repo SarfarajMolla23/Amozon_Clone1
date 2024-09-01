eb7279f621b488a912f993cfabda1d37e81e0970
SarfarajMolla <sarfarajmollah49@gmail.com>
2024-09-01 18:16:26 +0530

    Exercises 18 is done

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,7 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { loadProducts, loadProductsFetch } from "../data/products.js";
-import { loadCart } from "../data/cart.js";
+import { loadCart, loadCartFetch } from "../data/cart.js";
 
 //import '../data/car.js';
 //import '../data/cart-class.js';
@@ -10,21 +10,11 @@ import { loadCart } from "../data/cart.js";
 
 async function loadPage() {
   try {
-
     //throw 'error1';
-
-    await loadProductsFetch();
-
-    // reject() is function that's let us create an error in
-    // the future.
-    const value = await new Promise((resolve, reject) => {
-     // throw 'error';
-      loadCart(() => {
-        //reject('error3');
-        resolve();
-      });
-    });
-
+    await Promise.all([
+       loadProductsFetch(),
+        loadCartFetch()
+    ]);
   } catch (error) {
     console.log("Unexpected error . Please try again later ");
   }
