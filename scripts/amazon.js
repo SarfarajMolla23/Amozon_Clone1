0a4ef67fe5d3fc8771ba4d155de57dd15f2544d4
SarfarajMolla <sarfarajmollah49@gmail.com>
2024-09-11 18:11:12 +0530

    change orders and tracking

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -9,7 +9,29 @@ loadProducts(renderProductsGrid);
 function renderProductsGrid() {
   let productsHTML = "";
 
-  products.forEach((product) => {
+  const url = new URL(window.location.href);
+  const search = url.searchParams.get('search');
+
+  let filteredProducts = products;
+
+  // If a search exists in the URL parameters,
+  // filter the products that match the search.
+  if (search) {
+    filteredProducts = products.filter((product) => {
+      let matchingKeyword = false;
+
+      product.keywords.forEach((keyword) => {
+        if (keyword.toLowerCase().includes(search.toLowerCase())) {
+          matchingKeyword = true;
+        }
+      });
+
+      return matchingKeyword ||
+        product.name.toLowerCase().includes(search.toLowerCase());
+    });
+  }
+
+  filteredProducts.forEach((product) => {
     productsHTML += `
      <div class="product-container">
           <div class="product-image-container">
@@ -84,4 +106,10 @@ function renderProductsGrid() {
       updateCartQuantity();
     });
   });
-}
+
+document.querySelector('.js-search-button')
+    .addEventListener('click', () => {
+      const search = document.querySelector('.js-search-bar').value;
+      window.location.href = `amazon.html?search=${search}`;
+    });
+  }
